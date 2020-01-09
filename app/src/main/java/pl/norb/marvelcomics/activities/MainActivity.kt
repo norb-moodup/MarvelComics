25faf3bae6b32bb234e4d4ea801b3e3d856a4c7f
Norbert Hanysz <norberthanysz@gmail.com>
2020-01-09 16:56:30 +0100

    initial commit

---
+++ app/src/main/java/pl/norb/marvelcomics/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.norb.marvelcomics.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.norb.marvelcomics.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
